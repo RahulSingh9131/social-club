d7534671390d336e9e87e658dd33e8ffda0bef05
RAHUL SINGH <singh.rahul3111998vipin@gmail.com>
2022-05-17 17:06:52 +0530

    update : Added .env file

--- firebase.js
+++ firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDvUutfNnkO8MBtX_9J70shn6eg7vc0tu4",
-    authDomain: "social-club-c8402.firebaseapp.com",
-    projectId: "social-club-c8402",
-    storageBucket: "social-club-c8402.appspot.com",
-    messagingSenderId: "672717899808",
-    appId: "1:672717899808:web:4d7713ef0a010482f29445"
-};
+import { firebaseConfig } from "./.env";
 
 
 // Initialize Firebase
